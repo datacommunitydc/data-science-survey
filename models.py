3653d06351d4bd406f98bb09e8790240001984b0
Sean Murphy <murphsp1@gmail.com>
2012-10-15 18:12:43 -0400

    more db fix

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
+
+from cosmoquiz_app import app
+
+db = SQLAlchemy(app)
+
+class Result(db.Model):
+	__tablename__ = 'results'
+
+	id = db.Column(db.Integer, primary_key=True)
+	session_id = db.Column(db.Integer)
+	q1 = db.Column(db.String)
+	q2 = db.Column(db.String)
+	email = db.Column(db.String)
+
+
+	def __init__(self, q1, q2, session_id):
+		self.session_id = session_id
+		self.q1 = q1
+		self.q2 = q2
