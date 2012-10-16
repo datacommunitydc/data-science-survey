7f9d0a69d9a5e5d85189d229f59b629d40f01ba1
Sean Murphy <murphsp1@gmail.com>
2012-10-15 23:05:18 -0400

    styling changes

--- schema.sql
+++ schema.sql
@@ -4,6 +4,12 @@ create table results (
 	session_id integer not null,
 	q1 string not null,
 	q2 string not null,
-	email string null
+	email integer null
 );
 
+create table results (
+	session_id int not null,
+	q1 varchar not null,
+	q2 varchar not null,
+	email varchar null
+);
