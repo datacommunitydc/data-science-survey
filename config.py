4b240a52d53d1fc3337b70303a82364de5f4a804
Christian Ariza <christian.ariza@gmail.com>
2021-06-11 11:17:44 -0500

    [2to3] automatic migration

--- config.py
+++ config.py
@@ -1,21 +1,25 @@
 import os
 
+
 class Config(object):
-	DEBUG = False
-	TESTING = False
-	SECRET_KEY = '\xb7Fr\xbd7TQ\x7f9]\xafr\xf0\xa9\xe70Q\xbcY\xe8\xe8\x00v\xa6'
-	CSRF_ENABLED = True
-	#DATABASE_URI = 'sqlite:////tmp/badges.db'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/badges.db'
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "\xb7Fr\xbd7TQ\x7f9]\xafr\xf0\xa9\xe70Q\xbcY\xe8\xe8\x00v\xa6"
+    CSRF_ENABLED = True
+    # DATABASE_URI = 'sqlite:////tmp/badges.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/badges.db"
+
 
 class ProductionConfig(Config):
-	DEBUG = True
-	if 'DATABASE_URL' in os.environ.keys():
-		DATABASE_URI = os.environ['DATABASE_URL']
-		SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    DEBUG = True
+    if "DATABASE_URL" in list(os.environ.keys()):
+        DATABASE_URI = os.environ["DATABASE_URL"]
+        SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+
 
 class DevelopmentConfig(Config):
-	DEBUG = True
+    DEBUG = True
+
 
 class TestingConfig(Config):
-	TESTING = True
+    TESTING = True
--- models.py
+++ models.py
@@ -1,26 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
-#from flask.ext.sqlalchemy import SQLAlchemy
+
+# from flask.ext.sqlalchemy import SQLAlchemy
 # comment to make a change
 
 from cosmoquiz_app import app
 
 db = SQLAlchemy(app)
 
+
 class Result(db.Model):
-	__tablename__ = 'results'
+    __tablename__ = "results"
 
-	id = db.Column(db.Integer, primary_key=True)
-	session_id = db.Column(db.Integer)
-	q1 = db.Column(db.String)
-	q2 = db.Column(db.String)
-	email = db.Column(db.String)
-	date_added = db.Column(db.DateTime)
-	ip = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer)
+    q1 = db.Column(db.String)
+    q2 = db.Column(db.String)
+    email = db.Column(db.String)
+    date_added = db.Column(db.DateTime)
+    ip = db.Column(db.String)
 
-
-	def __init__(self, q1, q2, session_id, date_added, ip):
-		self.session_id = session_id
-		self.q1 = q1
-		self.q2 = q2
-		self.date_added = date_added
-		self.ip = ip
+    def __init__(self, q1, q2, session_id, date_added, ip):
+        self.session_id = session_id
+        self.q1 = q1
+        self.q2 = q2
+        self.date_added = date_added
+        self.ip = ip
